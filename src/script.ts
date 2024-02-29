4ebfe3560c7e2b7df7707287328c323d8cd76b31
Alessandro Garcia <alessandro.skull677@gmail.com>
2024-02-29 16:13:50 -0300

    Altered some scripts and env variables

--- src/script.ts
+++ src/script.ts
@@ -3,9 +3,12 @@ import { userRoutes } from "./routes/user.routes";
 import { videoRoutes } from "./routes/video.routes";
 
 const app = express();
+const port = process.env.PORT ?? 5000;
 
 app.use(express.json());
 app.use("/user", userRoutes);
 app.use("/videos", videoRoutes);
 
-app.listen(process.env.PORT);
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
